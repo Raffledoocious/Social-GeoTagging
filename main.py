dfcdbded8071b8d2f67afe8c3b799fbf756fe4ac
Sam Wirch <samwirch@gmail.com>
2011-12-10 16:57:58 -0600

    images are now stored in the photos objects
    main now parses the text files and stores the data in a data structure.
    Basic KML output is done.

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import os
 import glob
+# using the simplekml modules http://code.google.com/p/simplekml/
 import simplekml
 
+# make a list out of the lines of a text file
+def makeList(filename):
+	f = open(filename, 'r')
+	list = f.readlines()
+	f.close()
+	return list
+
+titlesList = makeList('images/titles.txt')
+latsList = makeList('images/lat.txt')
+lonsList = makeList('images/lon.txt')
+flickrurlsList = makeList('images/urls.txt')
 files = glob.glob('images/*.jpg')
 
+# holds all the info for the photos
+photos = []
+
+# populates a 2D array with the photo info
+# photos[0]['title'] is the first photo's title, etc.
+for i in range(len(titlesList)):
+	photos.append({'title':titlesList[i], 'lat':latsList[i], 'lon':lonsList[i], 'flickr':flickrurlsList[i], 'file':files[i]})
+
+# kml file
 kml = simplekml.Kml()
-kml.newpoint(name="test2", coords=[(18.432314,-33.988862)])
+
+for photo in photos:
+	kml.newpoint(name=photo['title'], coords=[(photo['lon'],photo['lat'])])
 kml.save("test.kml")
