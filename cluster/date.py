a33b61c4f3f7f6d0fd07dbcf47d496c7ab95bd9d
Jared Lutteke <jaydude898@gmail.com>
2011-12-14 00:41:50 -0600

    Updated date time shell

---
+++ cluster/date.py
@@ -0,0 +1,25 @@
+'''
+Parses out images based on date taken
+
+@author: Jared
+'''
+
+from datetime import date
+
+YEAR_OFFSET = 12
+HALF_YEAR = 6
+WEEK = 7
+
+
+def parse_date_clusters(photos):
+    """
+    Main parsing algorithm
+        
+    """
+    for photo in photos:
+        #get the datetime and parse out the photo
+        photo['time'] = date.today()
+    
+    return photos
+
+    
